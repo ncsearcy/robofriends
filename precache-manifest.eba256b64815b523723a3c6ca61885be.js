793d61a895ccd1fa0332f4d5ee72c076047e770e
Nicholas Searcy <ncsearcy@gmail.com>
2022-03-27 00:03:47 -0500

    Updates

---
+++ precache-manifest.eba256b64815b523723a3c6ca61885be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29498ad3c842ff30a863",
+    "url": "/robofriends/static/css/main.2cd1ada2.chunk.css"
+  },
+  {
+    "revision": "29498ad3c842ff30a863",
+    "url": "/robofriends/static/js/main.29498ad3.chunk.js"
+  },
+  {
+    "revision": "935b0251a626769e024b",
+    "url": "/robofriends/static/js/1.935b0251.chunk.js"
+  },
+  {
+    "revision": "dfde99143f350687472c",
+    "url": "/robofriends/static/css/2.f2b1d79f.chunk.css"
+  },
+  {
+    "revision": "dfde99143f350687472c",
+    "url": "/robofriends/static/js/2.dfde9914.chunk.js"
+  },
+  {
+    "revision": "97713e0cd9366aacb1d6",
+    "url": "/robofriends/static/js/runtime~main.97713e0c.js"
+  },
+  {
+    "revision": "d0e1e313a4280178b1f50782980bde4e",
+    "url": "/robofriends/index.html"
+  }
+];
